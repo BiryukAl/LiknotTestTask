8cd37233b524f85b7ba7201d1b6966c1b1252897
BiryukAl <kerzemyak001@gmail.com>
2023-08-09 11:37:50 +0300

    add ServiceLocator

--- app/src/main/java/ru/liknot/testtask/App.kt
+++ app/src/main/java/ru/liknot/testtask/App.kt
@@ -1,6 +1,31 @@
 package ru.liknot.testtask
 
 import android.app.Application
+import android.content.Context
+import ru.liknot.testtask.data.cache.SharedPreferencesImpl
+import ru.liknot.testtask.data.repository.RedirectRepositoryImpl
+import ru.liknot.testtask.di.ServiceLocator
+import ru.liknot.testtask.di.locate
+import ru.liknot.testtask.domain.repository.RedirectRepository
+import ru.liknot.testtask.domain.useCase.GetRedirectIdUseCase
+import ru.liknot.testtask.domain.useCase.SaveRedirectIdUseCase
 
 class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        ServiceLocator.register<Context>(this)
+
+        // Data
+        ServiceLocator.register<SharedPreferencesImpl>(locate())
+        ServiceLocator.register<RedirectRepository>(RedirectRepositoryImpl(locate()))
+
+        // Domain
+        ServiceLocator.register<GetRedirectIdUseCase>(GetRedirectIdUseCase(locate()))
+        ServiceLocator.register<SaveRedirectIdUseCase>(SaveRedirectIdUseCase(locate()))
+
+        // Presentation
+
+    }
 }
